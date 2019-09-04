70f9101e75ac23ee08b1d5e43575ea9012c9c1e0
John Otu <johnne01@gmail.com>
2019-09-04 12:36:34 +0100

    added linting and whitelisted lib folder

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 export const Sounder = (sound: string) => {
-  let count: number = 5, repeatedString: string = "";
+  let count: number = 5;
+  let repeatedString: string = '';
   while (count > 0) {
     repeatedString += sound;
     count--;
   }
   return repeatedString;
-}
+};
